df2ede6206b53c95f81ecd71b1c186b84ec0e0d2
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-06-30 09:23:20 +0530

    Time: 326 ms (5.05%), Space: 19.4 MB (48.97%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,15 +1,15 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         n = len(s)
-        frequencyMap = defaultdict(int)
+        frequency_map = {"a": 0, "b": 0, "c": 0}
         res = 0
         l = 0
 
         for r in range(n):
-            frequencyMap[s[r]] += 1
-            while l < r and all(frequencyMap[c] > 0 for c in ['a','b', 'c']):
-                res += n-r
-                frequencyMap[s[l]] -= 1
+            frequency_map[s[r]] += 1
+            while l < r and all(frequency_map[ch] > 0 for ch in "abc"):
+                res += n - r
+                frequency_map[s[l]] -= 1
                 l += 1
+
         return res
-            
