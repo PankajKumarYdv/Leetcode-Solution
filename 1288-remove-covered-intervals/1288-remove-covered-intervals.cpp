23a6ccca501bc491387647b0cbcf46f193e16cce
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-07-06 21:15:16 +0530

    7 ms (17.35%) 15.1 MB (60.13%) Medium 1288-remove-covered-intervals

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        auto compareIntervals = [](const std::vector<int>& left,
-                                   const std::vector<int>& right) {
-            return left[0] == right[0] ? left[1] > right[1]
-                                       : left[0] < right[0];
+        auto compareIntervals = [](const vector<int>& left, const vector<int>& right) {
+            return left[0] == right[0] ? left[1] > right[1]: left[0] < right[0];
         };
 
-        std::sort(intervals.begin(), intervals.end(), compareIntervals);
+        sort(intervals.begin(), intervals.end(), compareIntervals);
 
         int totalRemaining = intervals.size();
         int maxRightBoundary = 0;
 
         for (const auto& current : intervals) {
-            // If the current interval is enclosed within a previous larger
-            // interval
             if (current[1] <= maxRightBoundary) {
                 totalRemaining--;
             } else {
