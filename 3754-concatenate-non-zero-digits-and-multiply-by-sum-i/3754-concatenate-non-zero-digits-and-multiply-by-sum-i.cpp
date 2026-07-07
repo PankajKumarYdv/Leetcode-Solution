e8a7affec8502039dd9e27ec927ba1b30811733c
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-07-07 22:12:58 +0530

    0 ms (100%) 8.8 MB (89.09%) Easy 3754-concatenate-non-zero-digits-and-multiply-by-sum-i

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        long long x = 0; 
-        int num = n; 
-        int sum = 0; 
-        while(num > 0){
-            int rem = num % 10; 
-            sum += rem; 
-            num = num / 10; 
-            if(rem == 0) continue; 
-            x = x * 10 + rem;
+        long long extracted = 0, place = 1, sum = 0;
+        
+        while (n > 0) {
+            int rem = n % 10;
+            sum += rem;
+            
+            if (rem != 0) {
+                extracted = rem * place + extracted; // Builds the number in original order
+                place *= 10;
+            }
+            n /= 10;
         }
-
-        long long reversed_number = 0;
-    
-        while (x != 0) {
-            int remainder = x % 10;
-            reversed_number = reversed_number * 10 + remainder;
-            x /= 10;
-        }
-
-        reversed_number = reversed_number * sum; 
-        return reversed_number; 
+        
+        return extracted * sum;
     }
 };
