7f3be226e93f9d79cee1afcb3b59224ec1617d87
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-07-10 21:09:22 +0530

    471 ms (11.84%) 420.9 MB (7.89%) Hard 3534-path-existence-queries-in-a-graph-ii

--- 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
@@ -10,26 +10,20 @@ public:
             return a.first < b.first;
         });
 
-        // Map each original index to its rank in the sorted order
         unordered_map<int,int> map;
         for(int i = 0; i < n; i++) {
             map[sorted[i].second] = i;
         }
-
-        // LOG = number of binary-lifting levels needed to cover n nodes
         int LOG = 1;
         while ((1 << LOG) < n) LOG++;
         vector<vector<int>> up(n, vector<int>(LOG));
-
-        // up[i][0] = farthest sorted index reachable in one hop from i
         int r = 0;
         for (int l = 0; l < n; l++) {
-            if (r < l) r = l;                        // window can't start behind l
+            if (r < l) r = l;                      
             while (r + 1 < n && sorted[r + 1].first - sorted[l].first <= maxDiff) r++;
-            up[l][0] = r;                            // r is now as far right as l can reach
+            up[l][0] = r;                         
         }
 
-        // Binary lifting table: up[ind][j] = node reached after 2^j hops from ind
         for (int j = 1; j < LOG; j++) {
             for (int ind = 0; ind < n; ind++) {
                 up[ind][j] = up[up[ind][j - 1]][j - 1];
@@ -42,29 +36,26 @@ public:
             int dest = map[queries[ind][1]];
 
             if(src > dest) {
-                swap(src, dest); // we want to travel from left to right
+                swap(src, dest); 
             }
 
             if(src == dest) {
-                ans[ind] = 0; // same node - no hops needed
+                ans[ind] = 0; 
                 continue;
             }
 
             int curr = src;
             int hops = 0;
-            // Greedily take the largest jump that still lands strictly before `dest`
             for (int j = LOG - 1; j >= 0; j--) {
                 if (up[curr][j] < dest) {
                     curr = up[curr][j];
                     hops += (1 << j);
                 }
             }
-
-            // One more hop from curr should cover `dest`; otherwise it's unreachable
             if (up[curr][0] >= dest) {
                 ans[ind] = hops + 1;
             } else {
-                ans[ind] = -1; // `dest` is unreachable from `src`
+                ans[ind] = -1; 
             }
         }
 
