57acb342b82063dd773c28a1b9838777ce2410dd
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-07-15 08:56:16 +0530

    0 ms (100%) 7.8 MB (38.39%) Easy 0509-fibonacci-number

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     
+    // vector <int> dp_array{40,-1};  
+    vector<int> dp_array = vector<int>(40, -1);
     int fib(int n) {
         if(n==0) return 0; 
         if(n==1) return 1; 
-        // vector <int> dp_array(31,-1);  
-        // if(dp_array[n] != -1) return dp_array[n];
+        if(dp_array[n] != -1) return dp_array[n];
         int result = fib(n-1) + fib(n-2);
-        // dp_array[n] = result; 
+        dp_array[n] = result; 
         return result; 
     }
 };
