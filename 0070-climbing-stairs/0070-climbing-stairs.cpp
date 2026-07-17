1612c252a949e75e8ff84eb1bf5aabe7337d5280
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-07-17 22:08:42 +0530

    0 ms (100%) 8.6 MB (37.78%) Easy 0070-climbing-stairs

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public: 
+/* Approach 1 
     vector<int> dp_array = vector <int> (46, -1); 
     int climbStairs(int n) {
         if(n < 0){
@@ -19,4 +20,23 @@ public:
 
         return  dp_array[n] = oneStep + twoStep;
     }
+
+*/ 
+
+    int climbStairs(int n) {
+        if(n == 1 || n == 2 || n == 3){
+            return n; 
+        }
+
+        vector <int> t(n+1); 
+
+        t[0] = 0; 
+        t[1] = 1; 
+        t[2] = 2; 
+        for(int i = 3; i <= n; i++){
+            t[i] = t[i-1] + t[i-2]; 
+        }
+        return t[n]; 
+    }
+
 };
