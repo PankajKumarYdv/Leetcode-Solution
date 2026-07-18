aa5a40ccac434e033e121cc54d3da1e73a626764
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-07-18 08:37:36 +0530

    0 ms (100%) 7.8 MB (84.78%) Medium 0991-broken-calculator

--- 0991-broken-calculator/0991-broken-calculator.cpp
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
-        int o = 0; 
-        if(startValue == target) return 0; 
-        while(startValue != target){
-            if(target % 2 == 0 && startValue < target){
-                target /= 2; 
-                o++; 
-            }else{
-                target++; 
-                o++; 
-            }
-            
+        if(startValue >= target){
+            return startValue - target; 
         }
-        return o; 
+
+        if(target % 2 == 0){
+            return 1 + brokenCalc(startValue, target/2); 
+        }
+        return 1 + brokenCalc(startValue, target + 1); 
     }
 };
