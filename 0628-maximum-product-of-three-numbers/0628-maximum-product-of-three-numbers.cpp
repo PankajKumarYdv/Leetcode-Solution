03f83b15f606293d0471e7a15ec5c711cee2821c
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-07-26 22:57:43 +0530

    Time: 14 ms (25.59%), Space: 31.4 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -5,6 +5,6 @@ public:
         int n = nums.size(); 
         int option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
         int option2 = nums[0] * nums[1] * nums[n - 1];
-        return std::max(option1, option2);
+        return max(option1, option2);
     }
 };
