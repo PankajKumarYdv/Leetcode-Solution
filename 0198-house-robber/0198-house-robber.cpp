bf5c8d02e2d13aea2e7ecf623175d42d95ff1c28
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-08-03 19:10:06 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* // Memorization Approach 
     int t[101]; 
     int solve(vector<int>& nums, int i, int n){
         if(i >= n) return 0; 
@@ -14,5 +15,24 @@ public:
         int n = nums.size(); 
         memset(t, -1, sizeof(t)); 
         return solve(nums, 0, n); 
+    }
+    */
+
+    // Bottom up Approach 
+    int rob(vector<int>& nums){
+        int n = nums.size(); 
+        if(n==1) return nums[0]; 
+
+        vector<int> t(n+1, 0); 
+
+        t[0] = 0; 
+        t[1] = nums[0]; 
+        for(int i = 2; i <= n; i++){
+            int steal = nums[i-1] + t[i-2]; 
+            int skip = t[i-1]; 
+            t[i] = max(steal, skip); 
+        }
+
+        return t[n]; 
     }
 };
