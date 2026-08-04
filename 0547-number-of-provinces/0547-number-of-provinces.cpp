2f4ed475906b52a2476a7aaf6218534209147658
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-08-04 22:07:05 +0530

    Time: 0 ms (100%), Space: 19.3 MB (82.94%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+    void dfs(int node,
+             vector<vector<int>>& matrix,
+             vector<bool>& visited)
+    {
         visited[node] = true;
 
-        for (int neighbour : adj[node]) {
-            if (!visited[neighbour]) {
-                dfs(neighbour, adj, visited);
+        int V = matrix.size();
+
+        for(int neighbour = 0; neighbour < V; neighbour++)
+        {
+            if(matrix[node][neighbour] == 1 &&
+               !visited[neighbour])
+            {
+                dfs(neighbour, matrix, visited);
             }
         }
     }
 
-    int findCircleNum(vector<vector<int>>& matrix) {
+    int findCircleNum(vector<vector<int>>& matrix)
+    {
         int V = matrix.size();
-
-        vector<vector<int>> adj(V);
-
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (i != j && matrix[i][j]) {
-                    adj[i].push_back(j);
-                }
-            }
-        }
 
         vector<bool> visited(V, false);
 
         int provinces = 0;
 
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
+        for(int i = 0; i < V; i++)
+        {
+            if(!visited[i])
+            {
                 provinces++;
-                dfs(i, adj, visited);
+                dfs(i, matrix, visited);
             }
         }
 
