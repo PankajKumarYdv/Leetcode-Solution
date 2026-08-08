6707e81d70172cdfbba40123b3d07e2ff467b511
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-08-08 17:50:14 +0530

    Time: 1 ms (46.03%), Space: 19.7 MB (50.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,65 @@
 class Solution {
 public:
-    void dfs(int node,
-             vector<vector<int>>& matrix,
-             vector<bool>& visited)
-    {
+    /*
+    void dfs(int node, vector<vector<int>>& matrix, vector<bool>& visited) {
         visited[node] = true;
 
         int V = matrix.size();
 
-        for(int neighbour = 0; neighbour < V; neighbour++)
-        {
-            if(matrix[node][neighbour] == 1 &&
-               !visited[neighbour])
-            {
+        for (int neighbour = 0; neighbour < V; neighbour++) {
+            if (matrix[node][neighbour] == 1 && !visited[neighbour]) {
                 dfs(neighbour, matrix, visited);
             }
         }
     }
 
-    int findCircleNum(vector<vector<int>>& matrix)
-    {
+    int findCircleNum(vector<vector<int>>& matrix) {
         int V = matrix.size();
 
         vector<bool> visited(V, false);
 
         int provinces = 0;
 
-        for(int i = 0; i < V; i++)
-        {
-            if(!visited[i])
-            {
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
                 provinces++;
                 dfs(i, matrix, visited);
+            }
+        }
+
+        return provinces;
+    }
+
+    */
+
+    // Using BFS
+    int findCircleNum(vector<vector<int>>& matrix) {
+        int V = matrix.size();
+
+        vector<bool> visited(V, false);
+
+        int provinces = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                provinces++;
+
+                queue<int> q;
+                q.push(i);
+                visited[i] = true;
+
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+
+                    for (int neighbour = 0; neighbour < V; neighbour++) {
+                        if (matrix[node][neighbour] == 1 &&
+                            !visited[neighbour]) {
+                            visited[neighbour] = true;
+                            q.push(neighbour);
+                        }
+                    }
+                }
             }
         }
 
