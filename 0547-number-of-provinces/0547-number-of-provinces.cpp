d285788995e6dc93bc4b464b391ffb73bbd99aaf
Pankaj Kumar <109275156+PankajKumarYdv@users.noreply.github.com>
2026-08-08 18:05:08 +0530

    Time: 0 ms (100%), Space: 20.7 MB (27.87%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    /*
+    /* // Using DFS in Adjacency Matrix
     void dfs(int node, vector<vector<int>>& matrix, vector<bool>& visited) {
         visited[node] = true;
 
@@ -32,7 +32,8 @@ public:
 
     */
 
-    // Using BFS
+    /*
+    // Using BFS in Adjacency Matrix
     int findCircleNum(vector<vector<int>>& matrix) {
         int V = matrix.size();
 
@@ -60,6 +61,105 @@ public:
                         }
                     }
                 }
+            }
+        }
+
+        return provinces;
+    }
+    */
+
+    /*
+    // Using DFS in Adjacency List
+    void bfs(int start, vector<vector<int>>& adj, vector<bool>& visited) {
+        queue<int> q;
+
+        q.push(start);
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int neighbour : adj[node]) {
+                if (!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    q.push(neighbour);
+                }
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+
+        vector<vector<int>> adj(V);
+
+        // Matrix -> Adjacency List
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (i != j && isConnected[i][j] == 1) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        vector<bool> visited(V, false);
+
+        int provinces = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                provinces++;
+
+                bfs(i, adj, visited);
+            }
+        }
+
+        return provinces;
+    }
+    */
+
+    // BFS on Adjacency List
+    void bfs(int start, vector<vector<int>>& adj, vector<bool>& visited) {
+        queue<int> q;
+
+        q.push(start);
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (int neighbour : adj[node]) {
+                if (!visited[neighbour]) {
+                    visited[neighbour] = true;
+                    q.push(neighbour);
+                }
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& matrix) {
+        int V = matrix.size();
+
+        vector<vector<int>> adj(V);
+
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (i != j && matrix[i][j]) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        vector<bool> visited(V, false);
+
+        int provinces = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                provinces++;
+                bfs(i, adj, visited);
             }
         }
 
